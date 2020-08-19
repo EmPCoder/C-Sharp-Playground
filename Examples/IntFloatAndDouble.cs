91660b30f64fc5a68fcec4894ce3ecc6d4e1b4ad
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-19 09:34:49 -0400

    Defined Ints,Doubles,Floats did sum calcs

---
+++ Examples/IntFloatAndDouble.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Examples
+{
+    public class IntFloatAndDouble
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 13;//integer with value of 13
+            double num2 = 12.45;//double with value of 12.45
+            int num3;//BC not initialized = 0 
+            double sum = num1 + num2;//Sum of num1 and num2
+
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sumD = d1 + d2;
+            // Use casting
+            //int divDandI = d1 / num2;
+
+            float f1 = 3.5f;//Floats must have f at end
+
+            num3 = 15;//Reassigns value of num3 to 15
+
+            //Console.WriteLine(num1);
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum);
+            Console.WriteLine("The sum of " + d1 + " and " + d2 + " is " + sumD);
+            Console.Read();
+        }
+    }
+}
