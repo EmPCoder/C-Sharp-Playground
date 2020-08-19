a57c3ba7eeaa384c1b5d2f2a37d1a42abbbe897b
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-19 14:45:17 -0400

    Completed String Challenge 2 (Finding Specified Char, and Concat Method)

---
+++ Examples/Challenge String and its Methods 2/StringChallenge2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Examples.Challenge_String_and_its_Methods_2
+{
+    public class StringChallenge2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string and press enter");
+            string enteredString = Console.ReadLine();
+
+            Console.WriteLine("Please enter the character you would like to search for from the above string and press enter");
+            char enteredStringChar = Console.ReadLine()[0];//Defines a char to be searched from the above entered string to be found
+
+            int searchIndex = enteredString.IndexOf(enteredStringChar);///Gets the index of the character from the string.
+            Console.WriteLine("Index of character {0} in string is {1}", enteredStringChar, searchIndex);//{0} Referes to entered Char, {1} refers to the string?
+
+            Console.WriteLine("Please Enter your First Name");//Prompts user for first Name
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your Last Name");//Prompts user for last Name
+            string lastName = Console.ReadLine();
+
+            string fullName = string.Concat(firstName, " ", lastName);//Concat the firstName and lastName var and assign to fullName
+
+            Console.WriteLine("Your full name is {0}", fullName);//Prints the full name to the console
+        }
+    }
+}
--- Examples/Challenge String and its Methods/StringChallenge1.cs
+++ Examples/Challenge String and its Methods/StringChallenge1.cs
@@ -8,18 +8,18 @@ namespace Examples.Challenge_String_and_its_Methods
         /// The purpose of this challenge was to familiarize with storing user input into a var and using various string methods to manipulate the data (i.e. ToUpper,ToLower,Trim,and Substring)
         /// </summary>
         /// <param name="args"></param>
-        static void Main(string[] args)
-        {
-            string userEnteredString;
-            Console.WriteLine("Please enter your name and press enter");
-            userEnteredString = Console.ReadLine();//Defines a string userEnteredString and assigns the value of whatever string the user enters above
+        //static void Main(string[] args)
+        //{
+        //    string userEnteredString;
+        //    Console.WriteLine("Please enter your name and press enter");
+        //    userEnteredString = Console.ReadLine();//Defines a string userEnteredString and assigns the value of whatever string the user enters above
 
-            Console.WriteLine("The string that you entered is => " + userEnteredString);
+        //    Console.WriteLine("The string that you entered is => " + userEnteredString);
 
-            Console.WriteLine("Upper Case : {0}", userEnteredString.ToUpper());//Takes the user entered string and converts it to uppercase
-            Console.WriteLine("Lower Case : {0}", userEnteredString.ToLower());//Takes the user entered string and converts it to lowercase
-            Console.WriteLine("Trimmed Value : {0}", userEnteredString.Trim());//Takes the user entered string and gets rid of any extra whitespace
-            Console.WriteLine("Substring Value : {0}", userEnteredString.Substring(0,5));//string will display chars 0-5 (first 6)
-        }
+        //    Console.WriteLine("Upper Case : {0}", userEnteredString.ToUpper());//Takes the user entered string and converts it to uppercase
+        //    Console.WriteLine("Lower Case : {0}", userEnteredString.ToLower());//Takes the user entered string and converts it to lowercase
+        //    Console.WriteLine("Trimmed Value : {0}", userEnteredString.Trim());//Takes the user entered string and gets rid of any extra whitespace
+        //    Console.WriteLine("Substring Value : {0}", userEnteredString.Substring(0,5));//string will display chars 0-5 (first 6)
+        //}
     }
 }
