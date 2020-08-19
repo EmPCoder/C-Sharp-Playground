ebb32c76c6670d222d15825bd057c65b715d2d36
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-19 15:20:18 -0400

    Completed Datatypes Challenge

--- Examples/Challenge String and its Methods 2/StringChallenge2.cs
+++ Examples/Challenge String and its Methods 2/StringChallenge2.cs
@@ -4,26 +4,30 @@ namespace Examples.Challenge_String_and_its_Methods_2
 {
     public class StringChallenge2
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Please enter a string and press enter");
-            string enteredString = Console.ReadLine();
+        /// <summary>
+        /// The purpopse of this was to familiarize with finding where a defined char would be in a user entered string, as well as using concatenation.
+        /// </summary>
+        /// <param name="args"></param>
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Please enter a string and press enter");
+        //    string enteredString = Console.ReadLine();
 
-            Console.WriteLine("Please enter the character you would like to search for from the above string and press enter");
-            char enteredStringChar = Console.ReadLine()[0];//Defines a char to be searched from the above entered string to be found
+        //    Console.WriteLine("Please enter the character you would like to search for from the above string and press enter");
+        //    char enteredStringChar = Console.ReadLine()[0];//Defines a char to be searched from the above entered string to be found
 
-            int searchIndex = enteredString.IndexOf(enteredStringChar);///Gets the index of the character from the string.
-            Console.WriteLine("Index of character {0} in string is {1}", enteredStringChar, searchIndex);//{0} Referes to entered Char, {1} refers to the string?
+        //    int searchIndex = enteredString.IndexOf(enteredStringChar);///Gets the index of the character from the string.
+        //    Console.WriteLine("Index of character {0} in string is {1}", enteredStringChar, searchIndex);//{0} Referes to entered Char, {1} refers to the string?
 
-            Console.WriteLine("Please Enter your First Name");//Prompts user for first Name
-            string firstName = Console.ReadLine();
+        //    Console.WriteLine("Please Enter your First Name");//Prompts user for first Name
+        //    string firstName = Console.ReadLine();
 
-            Console.WriteLine("Please enter your Last Name");//Prompts user for last Name
-            string lastName = Console.ReadLine();
+        //    Console.WriteLine("Please enter your Last Name");//Prompts user for last Name
+        //    string lastName = Console.ReadLine();
 
-            string fullName = string.Concat(firstName, " ", lastName);//Concat the firstName and lastName var and assign to fullName
+        //    string fullName = string.Concat(firstName, " ", lastName);//Concat the firstName and lastName var and assign to fullName
 
-            Console.WriteLine("Your full name is {0}", fullName);//Prints the full name to the console
-        }
+        //    Console.WriteLine("Your full name is {0}", fullName);//Prints the full name to the console
+        //}
     }
 }
