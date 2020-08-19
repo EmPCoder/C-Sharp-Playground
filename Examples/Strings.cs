5a48c87c53d21f64ecdc3e750b06ed40a9e1f3ee
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-19 10:09:42 -0400

    Commented out IntFloatAndDouble Explored some functionality of strings

---
+++ Examples/Strings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examples
+{
+    public class Strings
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Dennis";//Coding standard to use string instead of String
+
+            string message = "My name is " + myName;
+
+            string capsMessage = message.ToUpper();//Converts text of string message to all uppercase
+
+            string lowerMessage = message.ToLower();//Converts text of string message to all lowercase.
+
+            Console.WriteLine(message.Length);//Prints out length of message string
+            Console.WriteLine(lowerMessage);
+            Console.WriteLine(capsMessage);
+            Console.WriteLine("My name is " + myName);
+            Console.Read();
+        }
+    }
+}
