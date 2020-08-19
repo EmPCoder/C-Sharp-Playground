6e172716e8ca2968be7dfb1784df96860ddfdabf
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-19 13:27:11 -0400

    Displayed Parsing to Strings

--- Examples/ParsingStringToInt.cs
+++ Examples/ParsingStringToInt.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Examples
 {
@@ -8,7 +6,16 @@ namespace Examples
     {
         static void Main(string[] args)
         {
+            string myString = "15";
+            string mySecondString = "13";
+            int num1 = Int32.Parse(myString);//parses myString into an int
+            int num2 = Int32.Parse(mySecondString);
 
+            int resultInt = num1 + num2;//stores the result of the newly parsed ints into resultInt
+            string result = myString + mySecondString;//only prints the contents so 1513.
+
+            Console.WriteLine(resultInt);
+            Console.Read();
         }
     }
 }
