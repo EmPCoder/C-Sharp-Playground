52d0e27c5b9397fab0dc79909de41e15a229e49a
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-19 14:03:24 -0400

    Completed String Challenge 1

--- Examples/ParsingStringToInt.cs
+++ Examples/ParsingStringToInt.cs
@@ -4,18 +4,18 @@ namespace Examples
 {
     public class ParsingStringToInt
     {
-        static void Main(string[] args)
-        {
-            string myString = "15";
-            string mySecondString = "13";
-            int num1 = Int32.Parse(myString);//parses myString into an int
-            int num2 = Int32.Parse(mySecondString);
+        //static void Main(string[] args)
+        //{
+        //    string myString = "15";
+        //    string mySecondString = "13";
+        //    int num1 = Int32.Parse(myString);//parses myString into an int
+        //    int num2 = Int32.Parse(mySecondString);
 
-            int resultInt = num1 + num2;//stores the result of the newly parsed ints into resultInt
-            string result = myString + mySecondString;//only prints the contents so 1513.
+        //    int resultInt = num1 + num2;//stores the result of the newly parsed ints into resultInt
+        //    string result = myString + mySecondString;//only prints the contents so 1513.
 
-            Console.WriteLine(resultInt);
-            Console.Read();
-        }
+        //    Console.WriteLine(resultInt);
+        //    Console.Read();
+        //}
     }
 }
