5423934165406856de8083c2a869fd4b0b997184
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-20 14:14:34 -0400

    Went Over try Catch and Tried to Divide by Zero

--- Methods/UserInput.cs
+++ Methods/UserInput.cs
@@ -6,51 +6,51 @@ namespace Methods
 {
     public class UserInput
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("please enter a string of text");
-            string input = Console.ReadLine();//Allows for user input and stores it in input var
-            Console.WriteLine(input);
-            //Console.Read();
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("please enter a string of text");
+        //    string input = Console.ReadLine();//Allows for user input and stores it in input var
+        //    Console.WriteLine(input);
+        //    //Console.Read();
 
-            Console.WriteLine(CalculateAdd());
-            Console.WriteLine(CalculateMultiply());
-        }
+        //    Console.WriteLine(CalculateAdd());
+        //    Console.WriteLine(CalculateMultiply());
+        //}
 
-        /**
-         * Method to Calculate addition two user inputted nums, first stores as strings then parses into ints to perform maths
-         */
-        public static int CalculateAdd()
-        {
-            Console.WriteLine("Please enter a number to be added");
-            string num1Input = Console.ReadLine();//Using ReadLine for strings
+        ///**
+        // * Method to Calculate addition two user inputted nums, first stores as strings then parses into ints to perform maths
+        // */
+        //public static int CalculateAdd()
+        //{
+        //    Console.WriteLine("Please enter a number to be added");
+        //    string num1Input = Console.ReadLine();//Using ReadLine for strings
 
-            Console.WriteLine("Please enter the second number to be added");
-            string num2Input = Console.ReadLine();
+        //    Console.WriteLine("Please enter the second number to be added");
+        //    string num2Input = Console.ReadLine();
 
-            int num1 = int.Parse(num1Input);
-            int num2 = int.Parse(num2Input);
+        //    int num1 = int.Parse(num1Input);
+        //    int num2 = int.Parse(num2Input);
 
-            int result = num1 + num2;
-            return result;
-        }
+        //    int result = num1 + num2;
+        //    return result;
+        //}
 
-        /**
-         * Method to Calculate Multiplication two user inputted nums, first stores as strings then parses into ints to perform maths
-         */
-        public static int CalculateMultiply()
-        {
-            Console.WriteLine("Please enter a number to be multiplied");
-            string num1Input = Console.ReadLine();
+        ///**
+        // * Method to Calculate Multiplication two user inputted nums, first stores as strings then parses into ints to perform maths
+        // */
+        //public static int CalculateMultiply()
+        //{
+        //    Console.WriteLine("Please enter a number to be multiplied");
+        //    string num1Input = Console.ReadLine();
 
-            Console.WriteLine("Please enter a second number to be multiplied");
-            string num2Input = Console.ReadLine();
+        //    Console.WriteLine("Please enter a second number to be multiplied");
+        //    string num2Input = Console.ReadLine();
 
-            int num1 = int.Parse(num1Input);
-            int num2 = int.Parse(num2Input);
+        //    int num1 = int.Parse(num1Input);
+        //    int num2 = int.Parse(num2Input);
 
-            int result = num1 * num2;
-            return result;
-        }
+        //    int result = num1 * num2;
+        //    return result;
+        //}
     }
 }
