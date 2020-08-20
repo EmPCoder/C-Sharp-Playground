7dea2ffd44d0b8b45c812daa8b871c7017bcdec1
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-20 09:33:25 -0400

    In this branch we have gone over some basic syntax of Constants

---
+++ Examples/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Examples
+{
+    //Constants are immutable values which are known
+    //At compile time and do not change for the life of the program.
+    public class Constants
+    {
+        //constants as fields
+        const double PI = 3.14159265359;
+        const int weeks = 52, months = 12;
+
+        //Create a constant of type string with your birthday as its value
+        const string birthDay = "02/15/1995";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My birthday is always going to be: {0}", birthDay);
+        }
+    }
+}
--- Examples/Datatypes and Variables/DatatypesAndVarsChallenge.cs
+++ Examples/Datatypes and Variables/DatatypesAndVarsChallenge.cs
@@ -8,33 +8,33 @@ namespace Examples.Datatypes_and_Variables
         /// The purpose of this class was to show the different primative data types and how to parse a string to an int. 
         /// </summary>
         /// <param name="args"></param>
-        static void Main(string[] args)
-        {
-            bool testBool = true;
-            char testChar = 'b';
-            string testString = "Hello World";
-            byte testByte = 8;
-            short testShort = 45;
-            int testInt = 345;
-            double testDouble = 32.56;
-            float testFloat = 45.6235F;
-            long testLong = 765895;
+        //static void Main(string[] args)
+        //{
+        //    bool testBool = true;
+        //    char testChar = 'b';
+        //    string testString = "Hello World";
+        //    byte testByte = 8;
+        //    short testShort = 45;
+        //    int testInt = 345;
+        //    double testDouble = 32.56;
+        //    float testFloat = 45.6235F;
+        //    long testLong = 765895;
 
-            string firstString = "I Control Text";
-            string secondString = "52";
-            Int32.Parse(secondString);//Parses the above string into an int
+        //    string firstString = "I Control Text";
+        //    string secondString = "52";
+        //    Int32.Parse(secondString);//Parses the above string into an int
 
-            Console.WriteLine(secondString);
+        //    Console.WriteLine(secondString);
 
-            Console.WriteLine("The test bool value is " + testBool);
-            Console.WriteLine("The test chare value is " + testChar);
-            Console.WriteLine("The test string value is " + testString);
-            Console.WriteLine("The test byte value is " + testByte);
-            Console.WriteLine("The test short value is " + testShort);
-            Console.WriteLine("The test int value is " + testInt);
-            Console.WriteLine("The test double value is " + testDouble);
-            Console.WriteLine("The test float value is " + testFloat);
-            Console.WriteLine("The test long value is " + testLong);
-        }
+        //    Console.WriteLine("The test bool value is " + testBool);
+        //    Console.WriteLine("The test chare value is " + testChar);
+        //    Console.WriteLine("The test string value is " + testString);
+        //    Console.WriteLine("The test byte value is " + testByte);
+        //    Console.WriteLine("The test short value is " + testShort);
+        //    Console.WriteLine("The test int value is " + testInt);
+        //    Console.WriteLine("The test double value is " + testDouble);
+        //    Console.WriteLine("The test float value is " + testFloat);
+        //    Console.WriteLine("The test long value is " + testLong);
+        //}
     }
 }
