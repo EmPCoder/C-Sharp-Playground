3f652a661fef40cc488b59aa30efc468a973a55a
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-20 15:46:57 -0400

    Went over various Operators that are available and their types

---
+++ Methods/Operators.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Methods
+{
+    /// <summary>
+    /// The purpose of this class is to discuss the various operators and their types availabe to you.
+    /// </summary>
+    public class Operators
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
+
+            //Unary Operators
+            num3 = -num1;//Multiplies num1 value by -1
+            Console.WriteLine("num3 is {0}", num3);
+            
+
+            //Logical Operator
+            bool isSunny = true;
+            Console.WriteLine("is it sunny? {0}", !isSunny);//Negates the isSunny value of the bool above.
+
+            //Increments Operators
+            int num = 0;
+            num++;
+            Console.WriteLine("num is {0}", num++);//Post Increment will do the line after
+            Console.WriteLine("num is {0}", num);
+            Console.WriteLine("num is {0}", ++num);//Pre increment will do the same line
+
+            //Decrement Operators
+            num--;
+            Console.WriteLine("num is {0}", num--);//Post Decrement will do the line after
+            Console.WriteLine("num is {0}", num);
+            Console.WriteLine("num is {0}", --num);//Pre Decrement will do the same line
+
+            int result;
+
+            //5+3=8
+            result = num1 + num2;
+            Console.WriteLine("result of num1 + num2 is {0}", result);
+            //5-3=2
+            result = num1 - num2;
+            Console.WriteLine("result of num1 - num2 is {0}", result);
+            //5/3=1 because using ints
+            result = num1 / num2;
+            Console.WriteLine("result of num1 / num2 is {0}", result);
+            //5*3 = 15
+            result = num1 * num2;
+            Console.WriteLine("result of num1 * num2 is {0}", result);
+            //5%3 = 2 Displays the remainder
+            result = num1 - num2;
+            Console.WriteLine("result of num1 % num2 is {0}", result);
+
+            //Relational and Type Operators
+            bool isLower;
+            isLower = num1 < num2;//GT LT statements
+            Console.WriteLine("result of num1 < num2 is {0}", isLower);
+
+            //Equality Operator
+            bool isEqual;
+            isEqual = num1 == num2;
+            Console.WriteLine("result of num1 == num2 is {0}", isEqual);
+
+            isEqual = num1 != num2;
+            Console.WriteLine("result of num1 != num2 is {0}", isEqual);
+
+            //Conditional Operators
+            bool isLowerandSunny;
+
+            //Condition1 AND Condition2
+            isLowerandSunny = isLower && isSunny;//Only true if both statements are true in this example using isSunny and isLower
+            Console.WriteLine("result of isLower && isSunny  is {0}", isLowerandSunny);
+
+            //Condition1 OR Condition2
+            isLowerandSunny = isLower || isSunny;
+            Console.WriteLine("result of isLower || isSunny  is {0}", isLowerandSunny);
+
+            Console.ReadKey();
+        }
+    }
+}
--- Methods/TryCatchAndFinally.cs
+++ Methods/TryCatchAndFinally.cs
@@ -2,47 +2,50 @@
 
 namespace Methods
 {
+    /// <summary>
+    /// The purpose of this class is to demo try,catch and finally blocks 
+    /// </summary>
     public class TryCatchAndFinally
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Please enter a number!");
-            string userInput = Console.ReadLine();
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Please enter a number!");
+        //    string userInput = Console.ReadLine();
 
-            int num1 = 5;
-            int num2 = 0;
-            int result;
-            try
-            {
-                result = num1 / num2;
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Can't Divide by Zero!");
-            }
+        //    int num1 = 5;
+        //    int num2 = 0;
+        //    int result;
+        //    try
+        //    {
+        //        result = num1 / num2;
+        //    }
+        //    catch (DivideByZeroException)
+        //    {
+        //        Console.WriteLine("Can't Divide by Zero!");
+        //    }
 
-            try
-            {
-                int userInputAsInt = int.Parse(userInput);
-            }
-            catch (FormatException)//Catch with Exception = Very Generic Error
-            {
-                Console.WriteLine("Format Expection, Please enter only numbers next time!");
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("Overflow Expection, Please enter a smaller number");
-            }
-            catch (ArgumentNullException) 
-            { 
-                Console.WriteLine("ArgumentNullException, the value was empty(null)");
-            }
-            finally//Code entered here will always be ran regardless
-            {
-                Console.WriteLine("This is called anyways");
-            }
+        //    try
+        //    {
+        //        int userInputAsInt = int.Parse(userInput);
+        //    }
+        //    catch (FormatException)//Catch with Exception = Very Generic Error
+        //    {
+        //        Console.WriteLine("Format Expection, Please enter only numbers next time!");
+        //    }
+        //    catch (OverflowException)
+        //    {
+        //        Console.WriteLine("Overflow Expection, Please enter a smaller number");
+        //    }
+        //    catch (ArgumentNullException)
+        //    {
+        //        Console.WriteLine("ArgumentNullException, the value was empty(null)");
+        //    }
+        //    finally//Code entered here will always be ran regardless
+        //    {
+        //        Console.WriteLine("This is called anyways");
+        //    }
 
-            Console.ReadKey();
-        }
+        //    Console.ReadKey();
+        //}
     }
 }
