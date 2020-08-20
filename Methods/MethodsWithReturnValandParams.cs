bf5efce4491e644506ee0223a8d9333e3977c4b1
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-20 10:56:57 -0400

    Discussed Methods with Return Types

---
+++ Methods/MethodsWithReturnValandParams.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    public class MethodsWithReturnValandParams
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(Add(1, 2), Add(3, 4)));//Would be 3 + 7 = 10 
+            Console.WriteLine(Add(15, 31));//Using Method as Argument when printing to console
+            Console.WriteLine(Multiply(56, 5));
+            Console.WriteLine(Divide(56, 13));
+            Console.Read();
+        }
+
+        //Method to add two ints together
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        //Method to multiply two ints together
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        //Method to Divide two ints 
+        public static double Divide(double num1, double num2)//Params have to be doubles as well if int will just return int value
+        {
+            return num1 / num2;
+        }
+    }
+}
--- Methods/VoidMethods.cs
+++ Methods/VoidMethods.cs
@@ -4,23 +4,22 @@ namespace Methods
 {
     public class VoidMethods
     {
-        static void Main(string[] args)
-        {
-            WriteSomething();//Calls our WriteSomething Method from below
-            WriteSomethingSpecific("I am an argument and am Called from a method");
-        }
+        //static void Main(string[] args)
+        //{
+        //    WriteSomething();//Calls our WriteSomething Method from below
+        //    WriteSomethingSpecific("I am an argument and am Called from a method");
+        //}
 
-        //Access modifier (static) return type method name (param1, param2)
-        public static void WriteSomething()//Added static so can be run in the main method
-        {
-            Console.WriteLine("I am called from a method");
-        }
+        ////Access modifier (static) return type method name (param1, param2)
+        //public static void WriteSomething()//Added static so can be run in the main method
+        //{
+        //    Console.WriteLine("I am called from a method");
+        //}
 
-        public static void WriteSomethingSpecific(string myText)
-        {
-            Console.WriteLine(myText);
-            Console.Read();
-        } 
-
+        //public static void WriteSomethingSpecific(string myText)
+        //{
+        //    Console.WriteLine(myText);
+        //    Console.Read();
+        //} 
     }
 }
