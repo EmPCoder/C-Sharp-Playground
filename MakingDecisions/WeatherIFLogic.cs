20f9f34f991d13295bb0decae0ab444c31ad40e2
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-31 13:05:03 -0400

    Defined a program to tell you what to do under certain weather basic IF

---
+++ MakingDecisions/WeatherIFLogic.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakingDecisions
+{
+    class WeatherIFLogic
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whats the temperature like? => ");
+
+            string temp = Console.ReadLine();
+            int temperature;
+            int number;
+            bool userEnteredANumber = int.TryParse(temp, out number);
+            /*
+             * Attempts to parse the user input if not a num defaults to 0
+             */
+            if (userEnteredANumber)//Replaces int.Parse(temp)...
+            {
+                temperature = number;
+            }
+            else
+            {
+                temperature = 0;
+                Console.WriteLine("The Value that you entered was not a number. 0 was set as temperature");
+            }
+
+            if (temperature < 10)//If temp < 10 do...
+            {
+                Console.WriteLine("Take the coat!");
+            }
+
+            else if (temperature == 10)//If temp == 10 do...
+            {
+                Console.WriteLine("It's 10 degrees C");
+            }
+
+            else //If temp > 10 do...
+            {
+                Console.WriteLine("cozy warm!");
+            }
+
+            Console.Read();
+        }
+    }
+}
