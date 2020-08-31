83392f0c341b2320da0e014cf53359df3b872ac7
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-31 16:17:57 -0400

    Completed the first IF Statement Challenge Register/Log In

---
+++ MakingDecisions/IF Statement Challenge 1/IFStatementChallenge1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakingDecisions.IF_Statement_Challenge_1
+{
+    class IFStatementChallenge1
+    {
+        //static void Main(string[] args)
+        //{
+        //    bool isRegistered = true;
+
+        //    Console.WriteLine("Please enter your firstName");
+        //    string fName = Console.ReadLine();
+        //    Console.WriteLine("Please enter your lastName");
+        //    string lName = Console.ReadLine();
+
+        //    if (fName != null && isRegistered == true)
+        //    {
+        //        Console.WriteLine(fName);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Please enter your username");
+        //    }
+
+        //    if (lName != null && isRegistered == true)
+        //    {
+        //        Console.WriteLine(lName);
+        //    }
+
+        //    string userName = fName + lName;
+
+        //    Console.WriteLine("Congratulations " + userName + " You are logged in!");
+        //}
+    }
+}
--- MakingDecisions/NestedIFStatements.cs
+++ MakingDecisions/NestedIFStatements.cs
@@ -6,29 +6,29 @@ namespace MakingDecisions
 {
     class NestedIFStatements
     {
-        static void Main(string[] args)
-        {
-            bool isAdmin = false;
-            bool isRegistered = true;
-            string userName = "";
-            Console.WriteLine("Please enter your user name");
+        //static void Main(string[] args)
+        //{
+        //    bool isAdmin = false;
+        //    bool isRegistered = true;
+        //    string userName = "";
+        //    Console.WriteLine("Please enter your user name");
 
-            userName = Console.ReadLine();
-            if (isRegistered && userName != "" && userName.Equals("admin"))
-            {
-                Console.WriteLine("Hi there, registered user");
-                //if(userName != "")//if userName != empty then do...
-                Console.WriteLine("Hi there, " + userName);//if userName not empty type "Hi there, userName...."
-                //if (userName.Equals("admin"))        
-                Console.WriteLine("Hi there, Admin");               
-            }
+        //    userName = Console.ReadLine();
+        //    if (isRegistered && userName != "" && userName.Equals("admin"))
+        //    {
+        //        Console.WriteLine("Hi there, registered user");
+        //        //if(userName != "")//if userName != empty then do...
+        //        Console.WriteLine("Hi there, " + userName);//if userName not empty type "Hi there, userName...."
+        //        //if (userName.Equals("admin"))        
+        //        Console.WriteLine("Hi there, Admin");               
+        //    }
 
-            if(isAdmin || isRegistered)
-            {
-                Console.WriteLine("You are logged in");
-            }
+        //    if(isAdmin || isRegistered)
+        //    {
+        //        Console.WriteLine("You are logged in");
+        //    }
 
-            Console.Read();
-        }
+        //    Console.Read();
+        //}
     }
 }
