b4e3c1d2d00d04dd589ec1438fc0f60b1c8820c6
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-31 10:13:06 -0400

    Created Simple Drinking & Voting Age test, as well as first exposure to if statements

---
+++ MakingDecisions/DrinkingVoting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakingDecisions
+{
+    //class DrinkingVoting
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Please enter your age");
+
+    //        string age = Console.ReadLine();//defines a var called age and assigns it to the user entered input
+    //        int ageTrue = Int32.Parse(age);//parses the user entered input from a string to an int
+
+    //        if (ageTrue < 18)//If user entered age is < 18 do...
+    //        {
+    //            Console.WriteLine("I'm sorry but you cant drink or Vote :(");
+    //        } 
+    //        else if (ageTrue >= 18 && ageTrue < 21)//If user entered age is >= 18 && < 21 do...
+    //        {
+    //            Console.WriteLine("You are able to vote, however you are not old enough to drink!");
+    //        }
+    //        else//If user entered age is > 21 do...
+    //        {
+    //            Console.WriteLine("Congratulations you are old enough to vote and drink!");
+    //        }
+    //    }
+    //}
+}
---
+++ MakingDecisions/IFLogic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakingDecisions
+{
+    class IFLogic
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whats the temperature like? => ");
+
+            string temp = Console.ReadLine();
+            int temperature = Int32.Parse(temp);
+
+            if (temperature < 10)
+            {
+                Console.WriteLine("Take the coat!");
+            }
+
+            if (temperature == 10)
+            {
+                Console.WriteLine("It's 10 degrees C");
+            }
+
+            if (temperature > 10)
+            {
+                Console.WriteLine("cozy warm!");
+            }
+
+            Console.Read();
+        }
+    }
+}
