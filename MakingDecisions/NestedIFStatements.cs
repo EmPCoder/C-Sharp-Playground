fe44ac9d92628d07d4f08bab105d2f3bad8c5f6d
Matthew Hudock <Matthewd1715@gmail.com>
2020-08-31 13:37:57 -0400

    Went over nested If Logic

---
+++ MakingDecisions/NestedIFStatements.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakingDecisions
+{
+    class NestedIFStatements
+    {
+        static void Main(string[] args)
+        {
+            bool isAdmin = false;
+            bool isRegistered = true;
+            string userName = "";
+            Console.WriteLine("Please enter your user name");
+
+            userName = Console.ReadLine();
+            if (isRegistered && userName != "" && userName.Equals("admin"))
+            {
+                Console.WriteLine("Hi there, registered user");
+                //if(userName != "")//if userName != empty then do...
+                Console.WriteLine("Hi there, " + userName);//if userName not empty type "Hi there, userName...."
+                //if (userName.Equals("admin"))        
+                Console.WriteLine("Hi there, Admin");               
+            }
+
+            if(isAdmin || isRegistered)
+            {
+                Console.WriteLine("You are logged in");
+            }
+
+            Console.Read();
+        }
+    }
+}
