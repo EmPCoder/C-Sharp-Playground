f1aead48bfaae9416255248d210f9d4411794db7
Matthew Hudock <Matthewd1715@gmail.com>
2020-09-02 15:57:10 -0400

    Covered SwitchStatements

--- MakingDecisions/IF Statement Challenge 1/IFStatementChallenge1.cs
+++ MakingDecisions/IF Statement Challenge 1/IFStatementChallenge1.cs
@@ -4,48 +4,48 @@ using System.Text;
 
 namespace MakingDecisions.IF_Statement_Challenge_1
 {
-    class IFStatementChallenge1
-    {
+    //class IFStatementChallenge1
+    //{
 
-        static string userName;
-        static string password;
+    //    static string userName;
+    //    static string password;
 
-        static void Main(string[] args)
-        {
-            Register();
-            LogIn();
-            Console.Read();
-        }
+    //    static void Main(string[] args)
+    //    {
+    //        Register();
+    //        LogIn();
+    //        Console.Read();
+    //    }
 
-        static void Register()
-        {
-            Console.WriteLine("Please enter your userName");
-            userName = Console.ReadLine();
-            Console.WriteLine("Please enter your password");
-            password = Console.ReadLine();
-            Console.WriteLine("Congratulations you have successfully Registered for our site!");
-            Console.WriteLine("----------------------------------------------------------------");
-        }
+    //    static void Register()
+    //    {
+    //        Console.WriteLine("Please enter your userName");
+    //        userName = Console.ReadLine();
+    //        Console.WriteLine("Please enter your password");
+    //        password = Console.ReadLine();
+    //        Console.WriteLine("Congratulations you have successfully Registered for our site!");
+    //        Console.WriteLine("----------------------------------------------------------------");
+    //    }
 
-        static void LogIn()
-        {
-            Console.WriteLine("Please enter your userName");
-            if (userName == Console.ReadLine())
-            {
-                Console.WriteLine("Please enter your password");
-                if (password == Console.ReadLine())
-                {
-                    Console.WriteLine("You have successfully logged in to our site :)");
-                }
-                else
-                {
-                    Console.WriteLine("Please enter the password that you registered with!");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Please enter the username that you registered with!");
-            }
-        }
-    }
+    //    static void LogIn()
+    //    {
+    //        Console.WriteLine("Please enter your userName");
+    //        if (userName == Console.ReadLine())
+    //        {
+    //            Console.WriteLine("Please enter your password");
+    //            if (password == Console.ReadLine())
+    //            {
+    //                Console.WriteLine("You have successfully logged in to our site :)");
+    //            }
+    //            else
+    //            {
+    //                Console.WriteLine("Please enter the password that you registered with!");
+    //            }
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Please enter the username that you registered with!");
+    //        }
+    //    }
+    //}
 }
---
+++ MakingDecisions/TempConvert.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakingDecisions
+{
+    //class TempConvert
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        FartoCel();
+    //    }
+
+    //    static void FartoCel()
+    //    {
+    //        Console.WriteLine("Please enter the temp to be converted to Cel");
+
+    //        string tempFar = Console.ReadLine();
+    //        int x = Int32.Parse(tempFar);
+
+    //        if (x > 32)
+    //        {
+    //            double tempFaren = (x - 32) * (.555);
+    //            Console.WriteLine(tempFaren); 
+    //        }           
+    //    }
+
+    //    static void CeltoFar()
+    //    {
+
+    //    }
+    //}
+}
