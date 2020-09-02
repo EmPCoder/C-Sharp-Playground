95a068e60c5aef97fa705ba4b91a680d284543de
Matthew Hudock <Matthewd1715@gmail.com>
2020-09-02 11:58:19 -0400

    Cleaned Up IFStatementChallenge1

--- MakingDecisions/IF Statement Challenge 1/IFStatementChallenge1.cs
+++ MakingDecisions/IF Statement Challenge 1/IFStatementChallenge1.cs
@@ -6,32 +6,46 @@ namespace MakingDecisions.IF_Statement_Challenge_1
 {
     class IFStatementChallenge1
     {
-        //static void Main(string[] args)
-        //{
-        //    bool isRegistered = true;
 
-        //    Console.WriteLine("Please enter your firstName");
-        //    string fName = Console.ReadLine();
-        //    Console.WriteLine("Please enter your lastName");
-        //    string lName = Console.ReadLine();
+        static string userName;
+        static string password;
 
-        //    if (fName != null && isRegistered == true)
-        //    {
-        //        Console.WriteLine(fName);
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Please enter your username");
-        //    }
+        static void Main(string[] args)
+        {
+            Register();
+            LogIn();
+            Console.Read();
+        }
 
-        //    if (lName != null && isRegistered == true)
-        //    {
-        //        Console.WriteLine(lName);
-        //    }
+        static void Register()
+        {
+            Console.WriteLine("Please enter your userName");
+            userName = Console.ReadLine();
+            Console.WriteLine("Please enter your password");
+            password = Console.ReadLine();
+            Console.WriteLine("Congratulations you have successfully Registered for our site!");
+            Console.WriteLine("----------------------------------------------------------------");
+        }
 
-        //    string userName = fName + lName;
-
-        //    Console.WriteLine("Congratulations " + userName + " You are logged in!");
-        //}
+        static void LogIn()
+        {
+            Console.WriteLine("Please enter your userName");
+            if (userName == Console.ReadLine())
+            {
+                Console.WriteLine("Please enter your password");
+                if (password == Console.ReadLine())
+                {
+                    Console.WriteLine("You have successfully logged in to our site :)");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter the password that you registered with!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter the username that you registered with!");
+            }
+        }
     }
 }
--- MakingDecisions/IF Statement Challenge 1/RegisteringAndLoggingIn.cs
+++ MakingDecisions/IF Statement Challenge 1/RegisteringAndLoggingIn.cs
@@ -4,48 +4,48 @@ using System.Text;
 
 namespace MakingDecisions.IF_Statement_Challenge_1
 {
-    class RegisteringAndLoggingIn
-    {
+    //class RegisteringAndLoggingIn
+    //{
 
-        static string userName;
-        static string password;
+    //    static string userName;
+    //    static string password;
 
-        static void Main(string[] args)
-        {
-            Register();//Calls Register Method
-            LogIn();//Calls Login Method.
-            Console.Read();
-        }
+    //    static void Main(string[] args)
+    //    {
+    //        Register();//Calls Register Method
+    //        LogIn();//Calls Login Method.
+    //        Console.Read();
+    //    }
 
-        public static void Register()//Method that handles All Registration Making static allows for use inside main
-        {
-            Console.WriteLine("Please enter your username");
-            userName = Console.ReadLine();
-            Console.WriteLine("Please enter your password");
-            password = Console.ReadLine();
-            Console.WriteLine("Registration Success!");
-            Console.WriteLine("----------------------------------------");
-        }
+    //    public static void Register()//Method that handles All Registration Making static allows for use inside main
+    //    {
+    //        Console.WriteLine("Please enter your username");
+    //        userName = Console.ReadLine();
+    //        Console.WriteLine("Please enter your password");
+    //        password = Console.ReadLine();
+    //        Console.WriteLine("Registration Success!");
+    //        Console.WriteLine("----------------------------------------");
+    //    }
 
-        public static void LogIn()
-        {
-            Console.WriteLine("Please enter your username");
-            if (userName == Console.ReadLine())//checks if correct username
-            {
-                Console.WriteLine("Please enter your password");
-                if(password == Console.ReadLine())//checks if correct password
-                {
-                    Console.WriteLine("Login Successful!");
-                }
-                else//entered wrong password
-                {
-                    Console.WriteLine("Login failed, wrong password. Reset Program");
-                }
-            }
-            else//entered wrong username
-            {
-                Console.WriteLine("Login failed, wrong password. Reset Program");
-            }
-        }
+    //    public static void LogIn()
+    //    {
+    //        Console.WriteLine("Please enter your username");
+    //        if (userName == Console.ReadLine())//checks if correct username
+    //        {
+    //            Console.WriteLine("Please enter your password");
+    //            if(password == Console.ReadLine())//checks if correct password
+    //            {
+    //                Console.WriteLine("Login Successful!");
+    //            }
+    //            else//entered wrong password
+    //            {
+    //                Console.WriteLine("Login failed, wrong password. Reset Program");
+    //            }
+    //        }
+    //        else//entered wrong username
+    //        {
+    //            Console.WriteLine("Login failed, wrong password. Reset Program");
+    //        }
+    //    }
     }
-}
+//}
