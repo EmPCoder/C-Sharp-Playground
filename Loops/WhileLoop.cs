2c2e796177086b85d9310eefb09971ecfa7e3126
Matthew Hudock <matthewd1715@gmail.com>
2021-01-13 15:18:33 -0500

    Demo'd program that would take in an empty string and increase count until user entered something else displays total at end

---
+++ Loops/WhileLoop.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class WhileLoop
+    {
+        static void Main(string[] args)
+        {
+            PeopleCounter();
+
+            int counter = 0;
+            //While Condition met runs code in loop
+            while(counter < 10)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+            Console.Read();
+        }
+
+        //While loop that allows user to press enter for an empty string and adds to the count per times pressed entered if user enters anything besides empty string ends program
+        public static void PeopleCounter()
+        {
+            int counter = 0;
+            string enteredText = "";
+
+            while(enteredText.Equals(""))
+            {
+                Console.WriteLine("Please press enter to increase amount by one and anything else + enter if you want to finish");
+                enteredText = Console.ReadLine();
+                counter++;
+                Console.WriteLine("current people count is {0}", counter);//{0} placeholder for counter var
+            }
+            Console.WriteLine("{0} people are inside the bus. Press enter to close the program", counter);
+            Console.Read();
+        }
+    }
+}
