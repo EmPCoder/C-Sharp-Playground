c8760c04439cac32efc4af3bdbb1e03bba0149b9
Matthew Hudock <matthewd1715@gmail.com>
2021-01-13 14:32:50 -0500

    Demo'd do while loops

---
+++ Loops/DoWhileLoops.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class DoWhileLoops
+    {
+        //static void Main(string[] args)
+        //{
+        //    int counter = 0;
+        //    int lengthOfText = 0;
+        //    string wholeText = "";
+        //    do
+        //    {
+        //        Console.WriteLine("Please enter the name of a friend");
+        //        string nameOfAFriend = Console.ReadLine();//Takes user input and stores as string nameOfAFriend
+        //        int currentLength = nameOfAFriend.Length;//checks length of above string
+        //        lengthOfText += currentLength;//adds about length to currentLenght
+        //        wholeText += nameOfAFriend;
+        //        //Console.WriteLine(counter);
+        //        //counter++;//Without this will loop infinitely
+        //    } while (lengthOfText < 20);
+        //    Console.WriteLine("Thanks that was enough! " + wholeText);
+        //    Console.Read();
+        //}
+    }
+}
