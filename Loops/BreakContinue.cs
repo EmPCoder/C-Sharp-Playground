0259f7900141e99979d11b207b6a929e67688ca0
Matthew Hudock <matthewd1715@gmail.com>
2021-01-13 15:27:01 -0500

    Demo'd break and continue

---
+++ Loops/BreakContinue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class BreakContinue
+    {
+        static void Main(string[] args)
+        {
+            for(int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine(counter);
+                if(counter %2 == 0)
+                {
+                    Console.WriteLine("Now comes an odd number");
+                    continue;
+                    //break;//Exits loop at given condition
+                }
+                Console.WriteLine(counter);
+            }
+            Console.Read();
+        }
+    }
+}
