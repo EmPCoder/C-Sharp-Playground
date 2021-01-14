b6fa46868293ea5bd3066bd311f49934c8460984
Matthew Hudock <matthewd1715@gmail.com>
2021-01-14 09:30:57 -0500

    Completed Loop Challenge 1

--- Loops/BreakContinue.cs
+++ Loops/BreakContinue.cs
@@ -6,22 +6,22 @@ using System.Threading.Tasks;
 
 namespace Loops
 {
-    class BreakContinue
-    {
-        static void Main(string[] args)
-        {
-            for(int counter = 0; counter < 10; counter++)
-            {
-                Console.WriteLine(counter);
-                if(counter %2 == 0)
-                {
-                    Console.WriteLine("Now comes an odd number");
-                    continue;
-                    //break;//Exits loop at given condition
-                }
-                Console.WriteLine(counter);
-            }
-            Console.Read();
-        }
-    }
+    //class BreakContinue
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        for(int counter = 0; counter < 10; counter++)
+    //        {
+    //            Console.WriteLine(counter);
+    //            if(counter %2 == 0)
+    //            {
+    //                Console.WriteLine("Now comes an odd number");
+    //                continue;
+    //                //break;//Exits loop at given condition
+    //            }
+    //            Console.WriteLine(counter);
+    //        }
+    //        Console.Read();
+    //    }
+    //}
 }
---
+++ Loops/Loops Challenge 1/LoopsChallenge1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops.Loops_Challenge_1
+{
+    class LoopsChallenge1
+    {
+        //static void Main(string[] args)
+        //{
+        //    AverageCalculator();
+        //}
+
+        //public static void AverageCalculator()
+        //{
+        //    string enteredText = "";
+        //    int grade;
+        //    int initialGrade = 0;
+        //    int counter = 0;
+        //    int gradeBeforeAverage = 0;
+
+        //    Console.WriteLine("Please use this program to calculate the average scores of your students press -1 when you are ready to exit");
+
+        //    try
+        //    {
+        //        do
+        //        {
+        //            Console.WriteLine("Please enter the {0} grade between 0 - 20", counter);
+                    
+        //                enteredText = Console.ReadLine();
+        //                counter++;
+        //                grade = Convert.ToInt32(enteredText);
+        //                gradeBeforeAverage = initialGrade += grade;
+        //                Console.WriteLine("The new total is {0} ", gradeBeforeAverage);
+
+        //            if (enteredText == "fuck")
+        //            {
+        //                break;
+        //            }
+
+        //        } while (enteredText != "-1");
+        //    }
+        //    catch (FormatException)
+        //    {
+        //        Console.WriteLine("Please enter only numbers between 1 and 20");
+        //    }
+
+        //    counter -= 1;
+
+        //    double avgGrade = (gradeBeforeAverage / counter);
+
+        //    Console.WriteLine("This students average grade is currently {0}", avgGrade);           
+                
+        //    Console.Read();
+        //}
+    }
+}
