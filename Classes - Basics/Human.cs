9494315390f16cbf8f02a835ef00718740c09986
Matthew Hudock <matthewd1715@gmail.com>
2021-01-29 12:22:35 -0500

    Demod the basics of OOP and created two public varibles to be used within two different objects calling the same method

---
+++ Classes - Basics/Human.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes___Basics
+{
+    //this class is a blueprint for a datatype
+
+    //create a public variable lastName of type string.
+    //change the IntroduceMyself method, so it tells the whole name.
+    //create two objects with full information about themself- firstName and lastName
+
+    class Human
+    {
+        //member variable
+        public string firstName;//by adding public allows outside class access
+
+        public string lastName;
+
+        //member
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, I'm {0}", firstName + lastName);
+        }
+    }
+}
---
+++ Classes - Basics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes___Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an object of class Human
+            //An instance of Human
+            Human denis = new Human();
+
+            //access public variable from outside, and even change it
+            denis.firstName = "Denis ";
+            denis.lastName = "Smith";
+
+            //call methods of the class
+            denis.IntroduceMyself();
+
+            Human michael = new Human();//Creating Human object, called michael
+
+            michael.firstName = "Michael ";//Setting firstName var defined in human as michael
+            michael.lastName = "Jones";
+            
+            michael.IntroduceMyself();//Calling IntroduceMyself method
+
+            Console.ReadKey();
+        }
+    }
+}
