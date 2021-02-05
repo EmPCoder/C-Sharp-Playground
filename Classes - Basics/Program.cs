8d5b35ef00ad8b3fc9fc7f0b2c8dc77181861c55
Matthew Hudock <matthewd1715@gmail.com>
2021-02-05 15:03:09 -0500

    Showed how to use a Deconstructor and Finalizer when getting rid of created object when necessary

--- Classes - Basics/Program.cs
+++ Classes - Basics/Program.cs
@@ -8,26 +8,26 @@ namespace Classes___Basics
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Box box = new Box(3,4,5);
+        //static void Main(string[] args)
+        //{
+        //    Box box = new Box(3,4,5);
 
-            ////box.length = 3;
-            ////box.SetLength(4); Because set Set Method to private this doesnt work
-            //box.SetLength(4);
-            ////box.height = 4;
-            //box.Height = 96;
-            //box.Width = 5;
-            Console.WriteLine("The box length is => " + box.GetLength());
-            Console.WriteLine("The box width is => " + box.Width);
-            box.Width = 10;//Updates the width
-            Console.WriteLine("The box width is => " + box.Width);
-            Console.WriteLine("The box Volume is => " + box.Volume);
-            Console.WriteLine("Front surface of the box is {0}", box.FrontSurface);
+        //    ////box.length = 3;
+        //    ////box.SetLength(4); Because set Set Method to private this doesnt work
+        //    //box.SetLength(4);
+        //    ////box.height = 4;
+        //    //box.Height = 96;
+        //    //box.Width = 5;
+        //    Console.WriteLine("The box length is => " + box.GetLength());
+        //    Console.WriteLine("The box width is => " + box.Width);
+        //    box.Width = 10;//Updates the width
+        //    Console.WriteLine("The box width is => " + box.Width);
+        //    Console.WriteLine("The box Volume is => " + box.Volume);
+        //    Console.WriteLine("Front surface of the box is {0}", box.FrontSurface);
 
-            //box.CalcFrontSurface();
-            box.DisplayInfo();
+        //    //box.CalcFrontSurface();
+        //    box.DisplayInfo();
 
-        }
+        //}
     }
 }
