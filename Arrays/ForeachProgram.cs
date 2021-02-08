338dc0abbea7d065749f2825e34cbf4f87fcfd0f
Matthew Hudock <matthewd1715@gmail.com>
2021-02-08 10:18:41 -0500

    Showed how foreach loops can loop through and perform certain operations on each element in an array

---
+++ Arrays/ForeachProgram.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class ForeachProgram
+    {
+        //static void Main(string[] args)
+        //{
+        //    int[] nums = new int[10];
+
+        //    // 0 -> 0
+        //    // 1 -> 1
+
+        //    for(int i = 0; i < 10; i++)
+        //    {
+        //        nums[i] = i + 10;//denotes it for starting at 10
+        //    }
+
+        //    //for each int in j prints out its position as well as the value
+        //    for(int j = 0; j < nums.Length; j++)
+        //    {
+        //        Console.WriteLine("Element {0} = {1}", j, nums[j]);
+        //    }
+
+        //    int counter = 0;
+        //    // create variable called k and run through whole array
+        //    foreach(int k in nums)
+        //    {
+        //        Console.WriteLine("Element {0} = {1}", counter, k); counter++;
+        //    }
+
+            
+        //    // create an array with 5 of your best friends
+        //    // create a foreach loop which greets all of them
+
+        //    string[] names = { "Matt", "Becca", "Brandon", "Dylan", "Tiger" };
+
+        //    foreach(string m in names)
+        //    {
+        //        Console.WriteLine("Hello {0} Good day aint it? ", m);
+        //    }
+
+        //    Console.ReadKey();
+        //}
+    }
+}
